49267684c68d1ef49d8d46c96e4e5ae234bbd0aa
LucasRossatto <lucas.sant.anagn3@gmail.com>
2025-02-02 20:35:55 -0300

    change(headerWithBackButton): adicionado novo style para icons e view

--- src/components/HeaderWithBackButton.tsx
+++ src/components/HeaderWithBackButton.tsx
@@ -10,16 +10,14 @@ export default function HeaderWithBackButton() {
     return (
         <View style={styles.header}>
             <View style={{flexDirection:"row",alignItems:"center",gap:10}}>
-            <TouchableOpacity onPress={() => router.back()} style={{ flexDirection: "row", gap: 10, alignItems: "center" }}>
-                <View style={styles.iconBackground}>
-                    <AntDesign name="left" size={18} color="white" />
-                </View>
+            <TouchableOpacity onPress={() => router.back()} style={{ flexDirection: "row", gap: 10, alignItems: "center", justifyContent:"center"}}>
+                    <AntDesign name="left" size={20} color="white" />
             </TouchableOpacity>
             <Text style={styles.title}>MyMovieList</Text>
             </View>
 
             <TouchableOpacity style={styles.iconBackground} onPress={() => router.push("/search")}>
-                <Octicons name="search" size={18} color="white" />
+                <Octicons name="search" size={20} color="white" />
             </TouchableOpacity>
         </View>
     );
@@ -31,7 +29,8 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         alignItems: "center",
         paddingVertical: 18,
-        paddingHorizontal: 30,
+        paddingRight: 30,
+        paddingLeft:20,
         backgroundColor: "#18191E",
     },
     title: {
